b1dfc930d03a2704b107791df49ad79f0f30b47d
brendon-boldt <brendon_boldt@yahoo.com>
2015-04-17 09:05:21 -0400

    Finishes heap sort

--- c/sort.c
+++ c/sort.c
@@ -6,11 +6,11 @@
 
 
 int main() {
-  int arr[] = {0,3,8,2,5,1,4};
+  int arr[] = {87, 48, 93, 20, 73, 29, 72, 13, 91, 24, 11, 75, 46, 34, 89, 49, 98, 67, 56, 37, 8, 31};
+  //           1 2   4       8 
   //int arr[] = {1,3,2};
   //int arr[] = {8,9,5};
   int length = sizeof(arr)/sizeof(int);
-
 
   printArray(arr,length);
   heapSort(arr,length);
--- heap-sort/heap-sort.c
+++ heap-sort/heap-sort.c
@@ -24,7 +24,7 @@ int maxPow2(int x) {
 }
 
 void sink(int index, int tree[], int length) {
-  printf("Sink: %d\n",tree[index]);
+  //printf("Sink: %d\n",tree[index]);
   if (index*2+1 < length) {
     if (index*2+2 < length) {
       if (leftChild(tree,index) > rightChild(tree,index)) {
@@ -51,30 +51,36 @@ void heapify(int end, int tree[], int length) {
   if (end < 2)
     return;
   for (int i = end; i > maxPow2(end); i -= 2) {
-    //printf("%d %d\n",tree[i],tree[i+1]);
     int max = 1;
-    if (tree[i+1] < tree[i])
+    if (tree[i+1] < tree[i] || i == length-1)
       max = 0;
     if (tree[i+max] > parent(tree,i)) {
       swap(&tree[i+max],&tree[(i-1)/2]);
       sink(i+max,tree,length);
     }
   }
-  printArray(tree,length);
+  //printArray(tree,length);
   heapify(maxPow2(end)+1,tree,length);
 }
 
 void buildTree(int tree[], int length) {
-  heapify(length,tree,length);
+  heapify(length-1,tree,length);
   //printArray(tree,length);
+}
+
+void sortHeap(int arr[], int length) {
+  int i = length - 1;
+  while (i > 0) {
+    swap(&arr[0],&arr[i]);
+    sink(0,arr,i--);
+    //printArray(arr,length);
+  }
 }
 
 
 void heapSort(int arr[], int length) {
-  // Remove malloc when finished
-  int * tree = malloc(length*sizeof(int));
-  memcpy(tree,arr,length*sizeof(int));
-  buildTree(tree,length);
-  printArray(tree,length);
-  free(tree);
+  buildTree(arr,length);
+  //printf("Heap: ");
+  //printArray(arr,length);
+  sortHeap(arr,length);
 }
